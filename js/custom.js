84625b9cac1d5dc9b7bc09f80ea2e32e3265a43a
Theo <theo.despoudis@teckro.com>
2019-12-29 12:48:14 +0000

    Updates

--- js/custom.js
+++ js/custom.js
@@ -17,45 +17,6 @@
         $('.dsec').html(event.strftime('%-S'));
     });
 
-    // Events Slider
-    var next_event_slider = new Swiper('.next-event-slider', {
-        slidesPerView: 4,
-        spaceBetween: 20,
-        loop: true,
-        centeredSlides: true,
-        breakpoints: {
-            // when window width is <= 320px
-            576: {
-                slidesPerView: 1
-            },
-            // when window width is <= 480px
-            768: {
-                slidesPerView: 2
-            },
-            // when window width is <= 640px
-            1200: {
-                slidesPerView: 3
-            }
-        },
-        navigation: {
-            nextEl: '.next-event-slider-wrap .swiper-button-next'
-        }
-    });
-
-    // Testimonial slider
-    var testimonialsSlider = new Swiper('.testimonials-slider', {
-        slidesPerView: 1,
-        spaceBetween: 0,
-        loop: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        navigation: {
-            nextEl: '.testimonials-container .swiper-button-next'
-        }
-    });
-
     // Tabs
     $(function() {
         $('.tab-content:first-child').show();
@@ -72,6 +33,9 @@
 
         $('.tab-nav:first-child').trigger('click');
     });
+
+    // Magnific Popup
+    $('.image-link').magnificPopup({type:'image'});
 
     // Accordion & Toggle
     $('.accordion-wrap.type-accordion').collapsible({
